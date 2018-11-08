5eb94dbb30065bacfd1e205d982936da54985f71
FurryFur <lance.e.chaney@gmail.com>
2018-11-08 18:46:37 +1300

    Setup for magic dataset.

---
+++ ml/wavegan/train_magic.bat
@@ -0,0 +1,20 @@
+python train_wavegan.py train .\train ^
+--data_dir .\data\Final_Datasets\Magic ^
+--data_first_window ^
+--use_extra_uncond_loss ^
+--wavegan_kernel_len 4 ^
+--wavegan_genr_upsample zeros ^
+--train_batch_size 32 ^
+--wavegan_disc_nupdates 1 ^
+--wavegan_batchnorm ^
+--use_disc_layernorm ^
+--train_summary_secs 15
+
+REM --use_extra_uncond_loss ^
+REM --wavegan_loss dcgan ^
+REM --wavegan_batchnorm ^
+REM --wavegan_disc_nupdates 1 ^
+REM --wavegan_genr_upsample nn ^
+REM --wavegan_kernel_len 8 ^
+REM --wavegan_disc_phaseshuffle 0 ^
+REM --wavegan_genr_pp ^
